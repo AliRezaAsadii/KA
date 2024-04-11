6674f25accdbb97f6abdd9105c26e37b168e817d
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-11 21:43:40 +0330

    create userRouter and usercontrollers

---
+++ controllers/userControllers.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const User = require("./../models/userModel");
+
+exports.getAllUser = () => {}
+
+exports.getUser = () => {}
+
+exports.createUser = () => {}
+
+exports.updateUser = () => {}
+
+exports.deleteUser = () => {}
+
+exports.loginUser = () => {}
