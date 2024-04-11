02d9b4abe0243ee0757686d9afcc8782f3772133
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-11 21:37:17 +0330

    create userModel

---
+++ models/userModel.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const { userSchema } = require("./../schema/userSchema");
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
