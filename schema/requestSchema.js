b13255d9ed94b55adf9dafc770b2f9d6711a8874
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-14 22:11:24 +0330

    create requestSchema

---
+++ schema/requestSchema.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A request most have name"],
+  },
+  title: {
+    type: String,
+    required: [true, "A request most have title"],
+  },
+  description: {
+    type: String,
+    required: [true, "A request most have title"],
+  },
+  requestDate: {
+    type: Date,
+  },
+  checkDate: {
+    type: Date,
+  },
+  status : {
+    type : Boolean,
+    default : false,
+  }
+});
--- schema/userSchema.js
+++ schema/userSchema.js
@@ -51,6 +51,10 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  token: {
+    type: Number,
+    default: 0,
+  },
 });
 
 exports.userSchema = userSchema;
