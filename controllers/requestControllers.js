810359b4f661b84735cffa1dacf2bc49bf1f9c3e
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-19 18:22:56 +0330

    finish getAllRequest function

--- controllers/requestControllers.js
+++ controllers/requestControllers.js
@@ -3,17 +3,17 @@
 const Request = require("./../models/requestModel");
 
 exports.getAllRequest = async (req, res) => {
-//   try {
-//     const requests = await Request.find();
+  try {
+    const requests = await Request.find();
 
-//     res.status(200).json({
-//       status: "success",
-//       result: requests.length,
-//       data: { requests },
-//     });
-//   } catch (err) {
-//     res.status(404).send(err);
-//   }
+    res.status(200).json({
+      status: "success",
+      result: requests.length,
+      data: { requests },
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
 };
 
 exports.getRequest = async (req, res) => {};
