18c8470fe161d17ac8f7c7c9e4d00556b45600f0
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-19 17:59:42 +0330

    create requestControllers.js

---
+++ controllers/requestControllers.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const Request = require("./../models/requestModel");
+
+exports.getAllRequest = async (req, res) => {};
+
+exports.getRequest = async (req, res) => {};
+
+exports.createRequest = async (req, res) => {};
+
+exports.updateRequest = async (req, res) => {};
+
+exports.deleteRequest = async (req, res) => {};
