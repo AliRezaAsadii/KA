595523ad165efe18e520886c1703fb50b9c4e33e
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-21 19:47:56 +0330

    create rewardSchema

---
+++ schema/rewardSchema.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const rewardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A request most have name"],
+  },
+  title: {
+    type: String,
+    required: [true, "A request most have title"],
+  },
+  description: {
+    type: String,
+    required: [true, "A request most have title"],
+  },
+  paidToken: {
+    type: String,
+    required: [true],
+  },
+  requestDate: {
+    type: Date,
+  },
+  checkDate: {
+    type: Date,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+exports.rewardSchema = rewardSchema;
