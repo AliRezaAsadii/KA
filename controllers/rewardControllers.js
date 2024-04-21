b11cc73c24e375de7bcb0aece547c0c29c2043a2
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-21 19:54:19 +0330

    create rewardControllers.js

---
+++ controllers/rewardControllers.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const Reward = require("./../models/rewardModel");
+
+exports.getAllReward = async (req, res) => {};
+
+exports.getReward = async (req, res) => {};
+
+exports.getAllReward = async (req, res) => {};
+
+exports.createReward = async (req, res) => {};
+
+exports.updateReward = async (req, res) => {};
+
+exports.deleteReward = async (req, res) => {};
