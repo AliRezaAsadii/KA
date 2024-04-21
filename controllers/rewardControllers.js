a0132459b2c6561c11d8c1d071cab20048c71dc8
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-21 20:14:10 +0330

    finish updateReward Function

--- controllers/rewardControllers.js
+++ controllers/rewardControllers.js
@@ -44,6 +44,25 @@ exports.createReward = async (req, res) => {
   }
 };
 
-exports.updateReward = async (req, res) => {};
+exports.updateReward = async (req, res) => {
+  try {
+    const reward = await Reward.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    console.log(reward);
+
+    res.status(200).json({
+      status: "success",
+      requestAt: req.requestTime,
+      data: {
+        reward,
+      },
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
 
 exports.deleteReward = async (req, res) => {};
