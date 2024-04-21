e8b573f5c6e600ceda845e11e732765b5ed388e4
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-21 20:08:39 +0330

    finish getReward Function

--- controllers/rewardControllers.js
+++ controllers/rewardControllers.js
@@ -16,7 +16,18 @@ exports.getAllReward = async (req, res) => {
   }
 };
 
-exports.getReward = async (req, res) => {};
+exports.getReward = async (req, res) => {
+  try {
+    const reward = await Reward.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: reward,
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
 
 exports.createReward = async (req, res) => {
   const newReward = await Reward.create(req.body);
